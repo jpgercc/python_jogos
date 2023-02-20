83d737c2aaf5834c424305c33d48fa34dbc5dd23
jpgercc <115590969+jpgercc@users.noreply.github.com>
2023-02-19 21:37:40 -0300

    Add files via upload
    The games are in portuguese and can be initiated by "jogos.py" or just by running the code directly, and it will run on python´s console.

---
+++ forca.py
@@ -0,0 +1,41 @@
+
+def jogar():
+
+    print("************************************")
+    print("Bem-vindo ao jogo de Forca!")
+    print("************************************")
+
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_", ]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros = erros + 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+    print("Fim do jogo.")
+
+if __name__ == "__main__":
+   jogar()
